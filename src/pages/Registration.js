ef19ed70fdc0b6a91778b746bb8c9f8159a4255c
AnkitPatanaik <apatanai@purdue.edu>
2017-09-25 22:06:27 -0400

    add input boxes

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,9 +1,53 @@
 import React from 'react'
 
 export default class Registration extends React.Component{
+  constructor(props) {
+          super(props);
+  }
+
   render(){
     return(
-      <div>Registration Page</div>
+      <div>
+        <center>
+        <div>Registration Page</div>
+        <form>
+          <input
+            text = "Email"
+            type = "text"
+            errorMessage = "email is invalid"
+            emptyMessage = "email can't be empty"
+          />
+
+          <input
+            text = "password"
+            type = "text"
+            errorMessage = "password is invalid"
+            emptyMessage = "password can't be empty"
+          />
+          
+          <input
+            text = "First Name"
+            type = "text"
+            errorMessage = "First name is invalid"
+            emptyMessage = "First name can't be empty"
+          />
+          
+          <input
+            text = "Last Name"
+            type = "text"
+            errorMessage = "Last name is invalid"
+            emptyMessage = "Last name can't be empty"
+          />
+
+          <button
+            type = "submit"
+            className = "button button_wide"
+            >
+            Register!
+          </button>
+        </form>
+        </center>
+      </div>
     );
   }
 }
