6dd0b7dc56588b4ef48bd81d43fdbc921792651e
Jay Hankins <jay.hankins@outlook.com>
2017-09-26 17:08:42 -0400

    Make necessary changes to bring our code standard up to Airbnb's ESLint preset.

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import RegistrationPage from './pages/RegistrationPage';
+
+ReactDOM.render(<RegistrationPage />, document.getElementById('root'));
