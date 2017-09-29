13e1e499aa017ba3c61c6fbbbe6dda114c2a1458
Jay Hankins <jay.hankins@outlook.com>
2017-09-28 20:59:52 -0400

    Add ruidmentary header to app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import HeaderComponent from '../components/HeaderComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <h2>Welcome to Lemming</h2>
-      </div>
+      <HeaderComponent />
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import '../node_modules/bulma/css/bulma.css';
+import './index.css';
 import App from './containers/App';
 import RegistrationPage from './containers/RegistrationPage';
 
