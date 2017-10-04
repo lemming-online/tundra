b29e2178562b66bc0dce5328699392e6ef29fc68
Jay Hankins <jhankins@salesforce.com>
2017-10-04 12:24:37 -0400

    Preliminary work with combined login/registration view

--- src/containers/RegistrationView.js
+++ src/containers/RegistrationView.js
@@ -56,21 +56,23 @@ class RegistrationView extends React.Component {
 
   render() {
     return (
-      <div className="tile is-child box">
-        <h1 className="title">Register</h1>
-        <form onSubmit={this.registerUser}>
-          <InputComponent title="Email" name="email" onChange={this.handleChange} />
-          <InputComponent title="Password" name="password" onChange={this.handleChange} />
-          <InputComponent title="First Name" name="first_name" onChange={this.handleChange} />
-          <InputComponent title="Last Name" name="last_name" onChange={this.handleChange} />
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Register</h1>
+          <form onSubmit={this.registerUser}>
+            <InputComponent title="Email" name="email" onChange={this.handleChange} />
+            <InputComponent title="Password" name="password" onChange={this.handleChange} />
+            <InputComponent title="First Name" name="first_name" onChange={this.handleChange} />
+            <InputComponent title="Last Name" name="last_name" onChange={this.handleChange} />
 
-          <div className="field">
-            <div className="control">
-              <button className="button is-primary">Register</button>
+            <div className="field">
+              <div className="control">
+                <button className="button is-primary">Register</button>
+              </div>
             </div>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+      </section>
     );
   }
 }
