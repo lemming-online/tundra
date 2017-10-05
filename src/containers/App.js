4710ef2e9ea644736e3aff91cf72c73f3fc69ead
Jay Hankins <jay@jayhankins.me>
2017-10-05 15:07:33 -0400

    Add guest login view to component page

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,12 +3,14 @@ import HeaderComponent from '../components/HeaderComponent';
 import './App.css';
 import SignInUpPage from './SignInUpPage';
 import StuffList from '../components/StuffList';
+import GuestLoginView from './GuestLoginView';
 
 function App() {
   return (
     <div className="App">
       <HeaderComponent />
       <SignInUpPage />
+      <GuestLoginView />
       <StuffList />
     </div>
   );
