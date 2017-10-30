ce942fd93e74ce0a30f329b9fcda023065e3f03c
AnkitPatanaik <apatanai@purdue.edu>
2017-10-29 22:45:45 -0400

    add some state change stuff

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,28 +1,34 @@
+import jwtDecode from 'jwt-decode';
 import * as types from '../actions/actionTypes';
 
 const INITIAL_STATE = {
-  isAuthenticated: !!localStorage.jwt,
+  isAuthenticated: false,
+  firstName: '',
+  lastName: '',
 };
 
 export default function loginReducer(state = INITIAL_STATE, action) {
+  const payload = JSON.stringify(jwtDecode(localStorage.jwt));
   switch (action.type) {
     case types.LOG_IN_SUCCESS:
       console.log('Login successful.');
+      console.log(`payload: ${payload}`);
+      console.log(`state: ${JSON.stringify(state)}`);
       return {
-        isAuthenticated: !!localStorage.jwt,
+        // ...state == the non explicitly stated parts of the state
+        ...state,
+        isAuthenticated: true,
+        firstName: localStorage.jwt.fnm,
+        lastName: localStorage.jwt.lnm,
       };
 
     case types.LOG_IN_FAILURE:
       console.log('Login failed.');
-      return {
-        isAuthenticated: !!localStorage.jwt,
-      };
+      return INITIAL_STATE;
 
     case types.LOG_OUT:
       console.log('Log out.');
-      return {
-        isAuthenticated: !!localStorage.jwt,
-      };
+      return INITIAL_STATE;
 
     default:
       return state;
--- src/reducers/registrationReducer.js
+++ src/reducers/registrationReducer.js
@@ -1,19 +1,24 @@
 import * as types from '../actions/actionTypes';
 
 const INITIAL_STATE = {
-  firstName: '',
-  lastName: '',
+  isRegistered: false,
 };
 
 export default function registrationReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case types.REGISTER_SUCCESS:
       console.log('Registration successful.');
-      return true;
+      return {
+        ...state,
+        isRegistered: true,
+      };
 
     case types.REGISTER_FAILURE:
       console.log('Registration failed.');
-      return false;
+      return {
+        ...state,
+        isRegistered: false,
+      };
 
     default:
       return state;
