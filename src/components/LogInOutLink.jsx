4a6381aaa61c006f8e832f11a023b135ba009a09
Jay Hankins <hankins.jay@gmail.com>
2017-10-31 17:42:35 -0400

    Dual login/out link

--- src/components/LogInOutLink.jsx
+++ src/components/LogInOutLink.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as loginActions from '../actions/loginActions';
 
 function LogInOutLink(props) {
+  function logOut(event) {
+    event.preventDefault();
+    props.actions.logOutUser();
+  }
+  // this.logOut = logOut.bind(this);
+
+  if (props.logged_in) {
+    return (
+      <Link to="/logout" onClick={logOut} className="navbar-item">
+        Log Out
+      </Link>
+    );
+  }
   return (
     <Link to="/login" className="navbar-item">
       Log In
     </Link>
   );
 }
+function mapStateToProps(state) {
+  return { logged_in: state.loginReducer.isAuthenticated };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(loginActions, dispatch),
+  };
+}
 
-export default LogInOutLink;
+export default connect(mapStateToProps, mapDispatchToProps)(LogInOutLink);
