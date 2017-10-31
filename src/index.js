3411311ce1f19ca92439f50b41b0f322df33d5d6
AnkitPatanaik <apatanai@purdue.edu>
2017-10-31 17:31:48 -0400

    fix draft-js import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import configureStore from './store/configureStore';
 import '../node_modules/bulma/css/bulma.css';
-import '../node_modules/draft-js/dist/Draft.css';
 import './index.css';
 
 import Root from './containers/Root';
