ed314d526789afd4ffb872433dbdd06afb0dd382
AnkitPatanaik <apatanai@purdue.edu>
2017-11-02 17:22:37 -0400

    hotfix

--- src/actions/announcementActions.js
+++ src/actions/announcementActions.js
@@ -22,10 +22,3 @@ export function closeAnnouncement(announcement) {
     console.log(`closing popup: ${announcement}`);
   };
 }
-
-export function announcementFailure() {
-  return function goFailAnnouncement(dispatch) {
-    dispatch(createAnnouncementFailure());
-    console.log('announcement failed');
-  };
-}
