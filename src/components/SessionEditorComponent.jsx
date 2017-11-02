9c99593904d0a826d99ac6d6706b9db464625587
AnkitPatanaik <apatanai@purdue.edu>
2017-11-02 14:43:01 -0400

    refactor session editor component

--- src/components/SessionEditorComponent.jsx
+++ src/components/SessionEditorComponent.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
-import { Editor, EditorState } from 'draft-js';
+import InputComponent from '../components/InputComponent';
 
 class SessionEditorComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { editorState: EditorState.createEmpty() };
-    this.onChange = editorState => this.setState({ editorState });
+    this.state = {
+      question: {
+        questionField: '',
+      },
+    };
   }
+
+  onChange = (event) => {
+    const nextQuestion = this.state.question;
+    nextQuestion[event.target.name] = event.target.value;
+    return this.setState({ question: nextQuestion });
+  };
+
+  onSubmit = () => {
+    console.log(this.state.question.questionField);
+    // push question to api through a session editor api class
+  };
+
   render() {
     return (
-      <div className="session-editor notification">
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+      <div>
+        <InputComponent
+          title="What do you need help with?"
+          name="questionField"
+          onChange={this.onChange}
+        />
+        <button onClick={this.onSubmit} className={'button is-primary'}>
+          Submit
+        </button>
       </div>
     );
   }
