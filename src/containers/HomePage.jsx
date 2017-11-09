e030f7b33e9d86dd7e5b5f26d0c55749c1005d29
Jay Hankins <hankins.jay@gmail.com>
2017-11-09 13:11:14 -0500

    Marketing nologin page

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -23,11 +23,7 @@ function loggedInPage(props) {
         </nav>
 
         <div className="columns is-multiline is-mobile">
-          <CourseCard />
-          <CourseCard />
-          <CourseCard />
-          <CourseCard />
-          <CourseCard />
+          <CourseCard courseName="FCK 420" professorName="Professor Fuckenstein" courseID="1" />
           <CourseCard />
         </div>
       </div>
@@ -37,35 +33,69 @@ function loggedInPage(props) {
 
 function loggedOutPage() {
   return (
-    <section className="hero is-primary is-large is-bold ">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title">Welcome to Lemming</h1>
-          <h2 className="subtitle">You are logged out.</h2>
-        </div>
-      </div>
-
-      <div className="hero-foot">
-        <nav className="tabs is-boxed is-fullwidth is-hidden-mobile">
-          <div className="container">
-            <ul>
-              <li className="is-active">
-                <a>Overview</a>
-              </li>
-              <li>
-                <a>For Students</a>
-              </li>
-              <li>
-                <a>For Instructors</a>
-              </li>
-              <li>
-                <a>For Events</a>
-              </li>
-            </ul>
+    <div>
+      <section className="hero is-primary is-fullheight is-bold ">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <h1 className="title">Welcome to Lemming</h1>
+            <h2 className="subtitle">You are logged out.</h2>
           </div>
-        </nav>
-      </div>
-    </section>
+        </div>
+
+        <div className="hero-foot">
+          <nav className="tabs is-boxed is-fullwidth is-hidden-mobile">
+            <div className="container">
+              <ul>
+                <li className="is-active">
+                  <a href="#overview">Overview</a>
+                </li>
+                <li>
+                  <a href="#students">For Students</a>
+                </li>
+                <li>
+                  <a href="#instructors">For Instructors</a>
+                </li>
+                <li>
+                  <a href="#events">For Events</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </section>
+      <section id="overview" className="hero is-light is-medium">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">Overview</h1>
+            <h2 className="subtitle">A new learning platform</h2>
+          </div>
+        </div>
+      </section>
+      <section id="students" className="hero is-info is-medium">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">For Students</h1>
+            <h2 className="subtitle">A new learning platform</h2>
+          </div>
+        </div>
+      </section>
+      <section id="instructors" className="hero is-warning is-medium">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">For Instructors</h1>
+            <h2 className="subtitle">A new learning platform</h2>
+          </div>
+        </div>
+      </section>
+      <section id="events" className="hero is-success is-bold is-medium">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">For Events</h1>
+            <h2 className="subtitle">A new learning platform</h2>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
