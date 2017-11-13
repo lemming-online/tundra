67b5202d03abb6a2113d6d8d653abe1e72b57bf5
Jay Hankins <hankins.jay@gmail.com>
2017-11-13 12:34:55 -0500

    Move jwt load into pageload

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import '../node_modules/bulma/css/bulma.css';
 import '../node_modules/animate.css/animate.min.css';
 import './index.css';
 
+import { LOG_IN_SUCCESS } from './actions/actionTypes';
+
 import Root from './containers/Root';
 
 const store = configureStore();
+
+// TODO: make this more robust with a reauth? or move that to
+// the authenticated request lib
+const jwt = localStorage.getItem('jwt');
+if (jwt) {
+  store.dispatch({ type: LOG_IN_SUCCESS });
+}
 
 // eslint-disable-next-line
 render(<Root store={store} />, document.getElementById('root'));
