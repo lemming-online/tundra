48749b7c50d4f5476f93ff7e0429ce205bb4a2b0
Jay Hankins <hankins.jay@gmail.com>
2017-11-21 10:28:22 -0500

    ughfsdafuhdsfads

--- src/containers/MeetingPage.jsx
+++ src/containers/MeetingPage.jsx
@@ -16,11 +16,9 @@ class SessionPage extends React.Component {
   render() {
     return (
       <div>
-        <section className="hero is-dark">
-          <div className="hero-body">
-            <div className="container">
-              <h1 className="title">Lab 3: Beginner Vaporization</h1>
-            </div>
+        <section className="section">
+          <div className="container">
+            <h1 className="title">Lab 3: Beginner Vaporization</h1>
           </div>
         </section>
 
