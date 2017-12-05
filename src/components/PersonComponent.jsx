65c6fad0abdd9da40a67e7c275043649d3c9b678
Jay Hankins <hankins.jay@gmail.com>
2017-12-05 11:27:29 -0500

    tooltip

--- src/components/PersonComponent.jsx
+++ src/components/PersonComponent.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import userBlank from '../images/user-blank.png';
 
+import '../../node_modules/bulma-extensions/bulma-tooltip/bulma-tooltip.css';
+
 function PersonComponent(props) {
   return (
     <div className="column is-2-desktop is-2-tablet is-half-mobile">
       <div className="instructor-component">
-        <figure className="image is-64x64">
+        <figure
+          className="image is-64x64 tooltip is-tooltip-right"
+          data-tooltip={'Location: Seat 3'}
+        >
           <img alt="instructor avatar" className="avatar is-circle" src={props.imgUrl} />
         </figure>
         <p className="has-text-centered hyphenate">{`${props.firstName} ${props.lastName}`}</p>
